113a05d959e76d22204bff69f0ebc6abd0eee2dc
virendra anjana <virendra.anjana0007@gmail.com>
2026-04-12 21:41:07 +0530

    Fix sitemap and robots URLs to use correct Vercel domain (refibreakeven.vercel.app)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://refibreakEven.com/sitemap.xml',
+    sitemap: 'https://refibreakeven.vercel.app/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://refibreakEven.com'
+  const baseUrl = 'https://refibreakeven.vercel.app'
 
   return [
     {
